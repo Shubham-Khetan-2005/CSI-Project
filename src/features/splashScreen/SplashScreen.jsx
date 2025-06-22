d4227c2085850d20ce614ef357d7e92d0138a2bf
Sparsh Rastogi <sparshh.sp26@gmail.com>
2025-06-22 15:35:40 +0530

    enhanced splashScreen

--- src/features/splashScreen/SplashScreen.jsx
+++ src/features/splashScreen/SplashScreen.jsx
@@ -1,8 +1,20 @@
 import React, { useState, useEffect } from 'react';
+import { BookUser, RadiationIcon } from 'lucide-react';
 import './splashScreen.css'; // for styling
 import GradientText from './GradientText';
 
 const SplashScreen = () => {
+  const Card = ({ title, Icon, strokeColor = "stroke-blue-400", children }) => (
+    <div className="bg-[#0b0110] border border-gray-700 rounded-3xl p-20 shadow-md space-y-2 hover:shadow-lg transition-all duration-300">
+      <div className="flex items-center gap-3 mb-2">
+        <Icon className={`w-6 h-6 ${strokeColor}`} />
+        <h2 className="text-lg font-semibold text-gray-400">{title}</h2>
+      </div>
+      <div className="text-sm text-gray-300 leading-relaxed">
+        {children}
+      </div>
+    </div>
+  );
   const [showSplash, setShowSplash] = useState(true);
   const [fadeOut, setFadeOut] = useState(false);
 
@@ -15,23 +27,26 @@ const SplashScreen = () => {
     };
   }, []);
 
-  return (
-    <div className="bg-black">
+  return (<>
+    <div className="bg-#000">
       {showSplash ? (
-        <div className={`splash-screen ${fadeOut ? 'fade-out' : ''}`}>
-          <GradientText
-            colors={["#08BDBD","#F8F8FF","#0D3B66"]}
-            animationSpeed={3}
+        <div className={`splash-screen bg-#000 ${fadeOut ? 'fade-out' : ''}`}>
+          <Card title="CSI-Project: Development Track" Icon={RadiationIcon} strokeColor="stroke-orange-400">
+           <GradientText
+            colors={["#FFFFFF","#111111"]}
+            animationSpeed={0}
             showBorder={false}
             className="custom-class"
             >
-            <h1>CSI Project: SDE-4</h1>
+            <h1>UCIL Awareness Portal</h1>
             </GradientText>
+          </Card>
         </div>
       ) : (
         <></>
       )}
     </div>
+    </>
   );
 }
 
